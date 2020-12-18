8d3783e388c9bf8f3893392c8387300dd12dfd10
hmogaki <hmogaki@ecbeing.co.jp>
2020-12-18 17:03:34 +0900

    こみっと

--- mabi_st/js/script.js
+++ mabi_st/js/script.js
@@ -6,7 +6,7 @@
 		return;
 	}
 	
-	var data = mabi_st.getAttribute("data-st");
+	var data = mabi_st.getAttribute("data-query");
 
 	data = {
 		"rywJ": "slc:0,race:H,style:blue,d:000h0y0P0*1e1v1M1)2b2s2J2!383p3G454m4D4U595k5B5R5_6gzvzMz)AbGaGsGIGZH76x6O6~7d7u7L8Z979o9La4alaCaTb1bibzbQcfcwcNc-d'e9y1EgEOE~HoKwKNK-LKeqeHeYf6fnfEfVgkgBgSh0hhhyhPh*ieiBiMi)jbjsjRj!CDCUD2DjLcLtkpkGkXl5lmlDlUm2mjmAmRm_ngnxnOn~ouoLo(paprpIFdFuFLF(pZq7qoqFqWr4rlrCrTs1sAsQs_tftwtNt-ucutuKu'DADRD_P0v9vqvHvYw6wEwVx3xkxBxSAJA!B8BpBGBXC5Cm9WyhyyyPy*zeNWO4OTHFHWI4IlITJ1JiJzJRJ_KgL(MaMrMZN7NoNFPhPyPPP*QeQvQ)RbRsRJR!S8SpSVT4TlTC"
